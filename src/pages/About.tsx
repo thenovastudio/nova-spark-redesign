055f1d615ab55e2872e4a42c4f44377108aaccdc
novastudio <matisse.petereyns1203@gmail.com>
2025-12-21 17:40:44 +0100

    Testing

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -80,7 +80,7 @@ const About = () => {
                 </div>
                 <div className="flex items-center gap-2 text-sm">
                   <Mail className="h-4 w-4 text-primary" />
-                  <span>hello@novastudio.be</span>
+                  <span>contact@thenovastudio.be</span>
                 </div>
               </div>
             </div>
